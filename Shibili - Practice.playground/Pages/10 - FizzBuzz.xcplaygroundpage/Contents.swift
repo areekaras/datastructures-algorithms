cb7e96de244a3b72a7380bc3ae3d78cb294d03ba
areekaras <shibili.areekara@gmail.com>
2022-03-07 20:12:15 +0530

    10 - FizzBuzz

---
+++ Shibili - Practice.playground/Pages/10 - FizzBuzz.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+
+func fizzBuzz(n: Int) {
+    for i in 1 ... n {
+        if i % 15 == 0 {
+            print("FizzBuzz")
+        } else if i % 3 == 0 {
+            print("Fizz")
+        } else if i % 5 == 0 {
+            print("Buzz")
+        } else {
+            print(i)
+        }
+    }
+}
+
+
+fizzBuzz(n: 15)
