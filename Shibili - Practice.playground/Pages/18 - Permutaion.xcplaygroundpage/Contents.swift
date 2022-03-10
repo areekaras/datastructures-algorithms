6ac834c165e316cbc58391dc16be77af5a0699ac
areekaras <shibili.areekara@gmail.com>
2022-03-10 11:14:42 +0530

    18 - Permutations

---
+++ Shibili - Practice.playground/Pages/18 - Permutaion.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*
+ ___                   _        _   _
+ | _ \___ _ _ _ __ _  _| |_ __ _| |_(_)___ _ _  ___
+ |  _/ -_) '_| '  \ || |  _/ _` |  _| / _ \ ' \(_-<
+ |_| \___|_| |_|_|_\_,_|\__\__,_|\__|_\___/_||_/__/
+ 
+ Challenge: Given two strings, check if one is a permutation of the other.
+ 
+ */
+
+func isPermutation(_ text: String, _ perm: String) -> Bool {
+    guard text.count == perm.count else { return false }
+    
+    return text.sorted() == perm.sorted()
+}
+
+isPermutation("abc", "cba") // true
+isPermutation("abc", "xyz") // false
